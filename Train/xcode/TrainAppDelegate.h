5ab08dbc2dd6f6248b6bd382cef3d3acf1dc72db
chenzhi <chenzhi13132601@gmail.com>
2011-05-05 02:08:44 +0800

    Merge branch 'master' of github.com:chenzhi/testGitHub

---
+++ Train/xcode/TrainAppDelegate.h
@@ -0,0 +1,18 @@
+//
+//  TrainAppDelegate.h
+//  Train
+//
+//  Created by 陈 志 on 11-5-5.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TrainAppDelegate : NSObject <UIApplicationDelegate> {
+    UIWindow *window;
+}
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@end
+
